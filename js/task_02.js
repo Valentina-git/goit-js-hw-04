183f0c988cfb375aff1e90580203610da3336b18
Valentina <dert12345f@ukr.net>
2020-11-25 17:56:35 +0200

    tasks

--- js/task_02.js
+++ js/task_02.js
@@ -2,7 +2,8 @@
  * Задание 2
 Исправь ошибки которые будут в консоли, чтобы скрипт заработал. */
 
-/**  const inventory = {
+
+const inventory = {
   items: ['Knife', 'Gas mask'],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
@@ -21,49 +22,15 @@ const invokeInventoryAction = function(itemName, action) {
   action(itemName);
 };
 
-invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
-console.log(inventory.items); // ['Knife', 'Medkit'] */
-//_________________________________________________________
-
-//Decision:
-
-// const inventory = {
-//   items: ['Knife', 'Gas mask'],
-//   add(itemName) {
-//     console.log(`Adding ${itemName} to inventory`);
-
-//     this.items.push(itemName);
-//   },
-//   remove(itemName) {
-//     console.log(`Removing ${itemName} from inventory`);
-
-//     this.items = this.items.filter(item => item !== itemName);
-//   },
-// };
-
-// const invokeInventoryAction = function(itemName, action) {
-//   console.log(`Invoking action on ${itemName}`);
-//   action(itemName);
-// };
-
-// invokeInventoryAction('Medkit', inventory.add.bind(inventory));
-// // Invoking action on Medkit
-// // Adding Medkit to inventory
-
-// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-
-// invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
-// // Invoking action on Gas mask
-// // Removing Gas mask from inventory
-
-// console.log(inventory.items); // ['Knife', 'Medkit']
+console.log(inventory.items); // ['Knife', 'Medkit']
 
